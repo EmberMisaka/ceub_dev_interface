798c0fe16b751b1d6ea3ee4c7ddceea3d778ad54
EmberMisaka <embermisaka@gmail.com>
2025-05-16 21:51:31 -0300

    Aula 07 e parte da 08

---
+++ Explicacoes/Aula_JavaScript/Parte01/Script.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 3;
+
+document.write("Soma: "+ (a+b) + "<br>");
+document.write("Sub: " +(a-b) + "<br>");
+document.write("Mult: " + (a*b) + "<br>");
+document.write("Div: " + (a/b) + "<br>");
+document.write("Rest: " + (a%b) + "<br>");
+document.write("Potência: " + (a**b) + "<br>");
+
+a += 5;
+b++;
+
+document.write("novo valor de a: " + a + "<br>");
+document.write("Novo valor de b: " + b  + "<br>");
